3e3ad84b2f92f75bba8250b1882468f046741ed2
shortnd <ococncol@gmail.com>
2021-08-15 18:57:19 -0400

    worked on making report query

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -4,10 +4,37 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  createEstimate({
+    make,
+    model,
+    lng,
+    lat,
+    year,
+    mileage,
+  }: GetEstimateDto): Promise<Report[]> {
+    return this.repo
+      .createQueryBuilder()
+      .select('AVG(price)', 'price')
+      .where('make = :make', {
+        make,
+      })
+      .andWhere('model = :model', {
+        model,
+      })
+      .andWhere('lng - :lng BETWEEN -5 AND 5', { lng })
+      .andWhere('lat - :lat BETWEEN -5 AND 5', { lat })
+      .andWhere('year - :year BETWEEN -3 AND 3', { year })
+      .orderBy('ABS(mileage - :mileage)', 'DESC')
+      .setParameters({ mileage })
+      .limit(3)
+      .getRawOne();
+  }
 
   create(body: CreateReportDto, user: User): Promise<Report> {
     const report = this.repo.create(body);
