7a74cc7cbe86bfb016aa9792c7843f34ce9023a3
shortnd <ococncol@gmail.com>
2021-08-15 16:28:43 -0400

    permissions systems

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { ReportDto } from './dtos/report.dto';
+import { Report } from './report.entity';
+import { ReportsService } from './reports.service';
+import { ApproveReportDto } from './dtos/approve-report.dto';
+import { AdminGuard } from '../guards/admin.guard';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {
+    console.log(query);
+  }
+
+  @Post()
+  @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
+  createReport(
+    @Body() body: CreateReportDto,
+    @CurrentUser() user: User,
+  ): Promise<Report> {
+    return this.reportsService.create(body, user);
+  }
+
+  @Patch('/:id')
+  @UseGuards(AdminGuard)
+  approveReport(
+    @Param('id') id: number,
+    @Body() body: ApproveReportDto,
+  ): Promise<Report> {
+    return this.reportsService.changeApproval(id, body.approved);
+  }
+}
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { User } from '../users/user.entity';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
+
+  create(body: CreateReportDto, user: User): Promise<Report> {
+    const report = this.repo.create(body);
+    report.user = user;
+    return this.repo.save(report);
+  }
+
+  async changeApproval(id: number, approved: boolean): Promise<Report> {
+    const report = await this.repo.findOne(id);
+    if (!report) {
+      throw new NotFoundException('Report not found');
+    }
+    report.approved = approved;
+    return this.repo.save(report);
+  }
+}
